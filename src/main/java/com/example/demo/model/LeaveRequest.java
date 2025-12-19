74a9c8447f2634a97ddfe22b32d78396c0364d41
bala-amypo <bala@amypo.in>
2025-12-19 08:00:40 +0000

    User commit

--- src/main/java/com/example/demo/model/LeaveRequest.java
+++ src/main/java/com/example/demo/model/LeaveRequest.java
@@ -1,5 +1,11 @@
 package com.example.demo.model;
 
 public class LeaveRequest {
-    private Long id
+    private Long id;
+    EmployeeProfile employee;
+    private Date startDate;
+    private Date endDate;
+    private String type;
+    private String status;
+    
 }
