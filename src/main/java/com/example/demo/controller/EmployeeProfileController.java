66ac1dca812dcb6bb2db47b840060a49fc587102
bala-amypo <bala@amypo.in>
2025-12-21 04:43:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/EmployeeProfileController.java
+++ src/main/java/com/example/demo/controller/EmployeeProfileController.java
@@ -15,12 +15,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
 
 @RestController
+@RequestMapping("/api/employees")
 public class EmployeeProfileController {
     @Autowired
     EmployeeService ser;
