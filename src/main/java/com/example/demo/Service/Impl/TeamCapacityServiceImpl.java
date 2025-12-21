89cd8a5f0baaed4a2d1426838823ddea31cb24ff
bala-amypo <bala@amypo.in>
2025-12-21 16:57:52 +0000

    User commit

--- src/main/java/com/example/demo/Service/Impl/TeamCapacityServiceImpl.java
+++ src/main/java/com/example/demo/Service/Impl/TeamCapacityServiceImpl.java
@@ -1,24 +1,22 @@
-    package com.example.demo.Service.Impl;
+package com.example.demo.Service.Impl;
 
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-    import com.example.demo.Service.TeamCapacityRuleService;
-    import com.example.demo.model.TeamCapacityRule;
-    import com.example.demo.repository.TeamCapacityConfigRepository;
-    @Service
-    public class TeamCapacityRuleImpl implements TeamCapacityRuleService {
-        @Autowired
-        TeamCapacityConfigRepository tccr;
+import com.example.demo.Service.TeamCapacityService;
+import com.example.demo.repository.UserAccountRepository;
 
-        @Override
-        public TeamCapacityRule create(TeamCapacityRule tcr){
-            return tccr.save(tcr);
-        }
-
-        @Override
-        public TeamCapacityRule getByTeam(String teamname){
-            return tccr.findByTeamName(teamname);
-        }
-
+@Service
+public class TeamCapacityServiceImpl implements TeamCapacityService {
+    @Autowired UserAccountRepository uar;
+    @Override
+    public String analyze(String teamName,java.sql.Date startDate,java.sql.Date endDate){
+        // Implementation logic here
+        return "Analysis result for team: " + teamName+", from "+startDate+" to "+endDate+" completed.";
     }
+    @Override
+    public String getAlert(String teamName){
+        // Implementation logic here
+        return "Your team: " + teamName+"has low capacity workers so leave request has been cancelled.";
+    }
+}
